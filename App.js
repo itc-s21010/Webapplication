d11251568936525de9b6352009d505783ff2d6c2
s21010 <s21010@std.it-college.ac.jp>
2021-11-10 12:53:30 +0000

    likebutton

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
-const INITIAL_COUNT = 0
-const LikeButton  = () => {
-  const [liked, setLiked] = (false);
-  const toggleLiked = () => setLiked(!liked);
+const LikeButton = () => {
+  const [liked, setLiked] = (false)
+  const toggleLiked = () => setLiked(!liked)
   return (
-      <button className="likeButton" onClick={toggleLiked}>
-    {liked ? "いいね済" : "いいね前"}
+    <button className='likeButton' onClick={toggleLiked}>
+    {liked ? 'いいね済' : 'いいね前'}
     </button>
   )
 }
-
 export default App
