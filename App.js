a6dd0920f8ed9424beb4b094fc371e0e6c0e8c69
s21010 <s21010@std.it-college.ac.jp>
2021-11-10 07:00:35 +0000

    likebutton

--- App.js
+++ App.js
@@ -2,18 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 
 const INITIAL_COUNT = 0
-const App = () => {
-  const [count, setCount] = useState(INITIAL_COUNT)
-  const countAdd = () => setCount(prevCount => prevCount + 1)
-  const countSub = () => setCount(prevCount => prevCount - 1)
-  const countReact = () => setCount(INITIAL_COUNT)
+const LikeButton  = () => {
+  const [liked, setLiked] = (false);
+  const toggleLiked = () => setLiked(!liked);
   return (
-    <>
-      <button onClick={countAdd}>ボタン +1</button>
-      <button onClick={countSub}>ボタン -1</button>
-      <button onClick={countReact}>リセット</button>
-      <p>カウント:{count}</p>
-    </>
+      <button className="likeButton" onClick={toggleLiked}>
+    {liked ? "いいね済" : "いいね前"}
+    </button>
   )
 }
 
