ed1eff309442317749e9280b0f7ffcf1c00373a0
itc-s21010 <s21010@std.it-college.ac.jp>
2021-12-10 06:04:57 +0000

    a

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import React, 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-const LikeButton = () => {
-  const [liked, setLiked] = (false)
-  const toggleLiked = () => setLiked(!liked)
-  return (
+const values = [
+  { id: 1, item: 'html' },
+  { id: 2, item: 'CSS' },
+  { id: 3, item: 'JavaScript' }
+]
 
-  <button className='likeButton' onClick={toggleLiked}>
-    {liked ? 'いいね済' : 'いいね前'}
-  </button>
+const SelectItems = values.map(value => {
+  return (
+    <option value={value.item} key={value.id}>
+      {value.item}
+    </option>
+  )
+})
+
+const InputSelectBox = () => {
+  const [selectedValue, setSelectedValue] = useState(values[0]['item'])
+
+  const handleChange = e => {
+    setSelectedValue(e.target.value)
+  }
+
+  return (
+    <div className='App'>
+      <p>
+        現在選択されている値：
+        <b>{selectedValue}</b>
+      </p>
+
+      <select value={selectedValue} onChange={handleChange}>
+        {SelectItems}
+      </select>
+    </div>
   )
 }
-export default App
+
+export default function App () {
+  return <InputSelectBox />
+}
