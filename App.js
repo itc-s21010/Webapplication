4224ed8e3a6087204898db605605c4975a7b5ed1
itc-s21010 <s21010@std.it-college.ac.jp>
2021-12-11 20:50:56 +0000

    a

--- App.js
+++ App.js
@@ -1,41 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import Header from './Header'
+import Movie from './Movie'
+import Search from './Search'
 
-const values = [
-  { id: 1, item: 'html' },
-  { id: 2, item: 'CSS' },
-  { id: 3, item: 'JavaScript' }
-]
+const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=4a3b711b'
 
-const SelectItems = values.map(value => {
-  return (
-    <option value={value.item} key={value.id}>
-      {value.item}
-    </option>
-  )
-})
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
 
-const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState(values[0]['item'])
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search)
+        setLoading(false)
+      })
+  }, [])
 
-  const handleChange = e => {
-    setSelectedValue(e.target.value)
+  const search = searchValue => {
+    setLoading(true)
+    setErrorMessage(null)
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === 'True') {
+          setMovies(jsonResponse.Search)
+          setLoading(false)
+        } else {
+          setErrorMessage(jsonResponse.Error)
+          setLoading(false)
+        }
+      })
   }
 
   return (
     <div className='App'>
-      <p>
-        現在選択されている値：
-        <b>{selectedValue}</b>
-      </p>
-
-      <select value={selectedValue} onChange={handleChange}>
-        {SelectItems}
-      </select>
+      <Header text='HOOKED' />
+      <Search search={search} />
+      <p className='App-intro'>Sharing a few of our favourite movies</p>
+      <div className='movies'>
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className='errorMessage'>{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   )
 }
 
-export default function App () {
-  return <InputSelectBox />
-}
+export default App
